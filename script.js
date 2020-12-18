411030e7013daa655272cd99c356a6d7e66dc399
Kevin Buttimer <kbuttimer4@gmail.com>
2020-12-18 16:08:19 -0500

    updating files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 console.log("Hello World");
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+
+// api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
