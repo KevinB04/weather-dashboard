a786d8273d39a30c1920883296968fc1562c6a63
Kevin Buttimer <kbuttimer4@gmail.com>
2020-12-21 15:45:46 -0500

    updating files

--- script.js
+++ script.js
@@ -1,97 +1,45 @@
- $(document).ready(function() {
+$(document).ready(function() {
 
-   console.log("Hello World");
+  $("#searchWeather").on("click", function(e){
+    e.preventDefault();
+    var city = $("#city-input").val().trim();
 
-   var APIKey = "f0f1cd985ea5ac5733a66416d91fac2f";
+    searchWeather(city)
+  })
 
-   var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={f0f1cd985ea5ac5733a66416d91fac2f}',
+  function searchWeather(name) {
+    console.log(name)
+    $.ajax({
+      method: "GET",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=f0f1cd985ea5ac5733a66416d91fac2f&units=imperial",
+      dataType: "json"
+    }).then(function(res){
+      console.log(res)
 
-   var lat = response.coord.lat;
-   var lon = response.coord.lon;
+      var card = $("<div>").addClass("card mt-5");
+      var cardBody = $("<div>").addClass("card-body");
+      var cardTitle = $("<h2>").addClass("card-title").text(res.name);
+      var image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + res.weather[0].icon + ".png")
 
-   $('#searchWeather').click(function(){
-   var city = $("#city-input").val();
-     if(city != ''){
-       $.ajax({
-         url: queryURL,
-         method: "GET",
-         dataType: "json",
-         success: function(data){
-           console.log(data);
-         }
-   });
-     }else{
-       $("#error").html('Field cannot be blank');
-     }
-     
+      $("#today").append(card.append(cardBody.append(cardTitle.append(image))))
 
-     if(currentWeather === "Clear") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if(currentWeather === "Thunderstorms") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/11d.png");
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Cloudy") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Mist") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/50d.png")
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Rain") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Snow") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Few Clouds") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/02d.png")
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Scattered Clouds") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png")
-       currentIcon.attr("style", "height: 60px, width: 60px");
-     } else if (currentWeather === "Tornado") {
-       var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/50d.png")
-       currentIcon.attr("style", "height: 60px, width: 60px");
+      searchForecast(res.coord.lat, res.coord.lon)
+    })
+  }
 
-     // DOM Variables
-  
-     // JavaScript Variables
-  
-     // Function Definitions
-  
-     // Function Calls
-  
-     // Event Listeners
+  function searchForecast(lat, lon) {
 
-     function alertCityName() {
-       var city = $(this).data("name");
-       alert(city);
-     }
+    $.ajax({
+      method: "GET",
+      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=f0f1cd985ea5ac5733a66416d91fac2f&units=imperial`,
+      dataType: "json"
+    }).then(function(res){
+      console.log(res)
+      for(var i = 1; i < 6; i++) {
+        console.log(res.daily[i])
+        // create cards for each day
+      }
+    })
+  }
 
-     function renderButtons() {
-
-       $("#submit").empty();
-
-       for (var i = 0; i < city.length; i++) {
-         var a = $("<button>");
-         a.addClass("city");
-         a.attr("data-name", city[i]);
-         a.text(city[i]);
-         $("submit").append(a);
-       }
-     }
-
-     $("#add-city").on("click", function(event) {
-       event.preventDefault();
-       var city = $("search").val().trim();
-       city.push(city);
-       renderButtons();
-     });
-
-
-
-       $(" .description").val(localStorage.getItem(" "))
-
-     $(document).on("click", ".city", alertCityName);
-
-     renderButtons();
+})
