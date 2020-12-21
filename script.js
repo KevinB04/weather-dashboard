95ee9411c3d65a352c7778dac1919602ab834ba3
Kevin Buttimer <kbuttimer4@gmail.com>
2020-12-20 21:57:29 -0500

    updating files

--- script.js
+++ script.js
@@ -1,77 +1,77 @@
-$(document).ready(function() {
+ $(document).ready(function() {
 
-  console.log("Hello World");
+   console.log("Hello World");
 
-  var APIKey = "f0f1cd985ea5ac5733a66416d91fac2f";
+   var APIKey = "f0f1cd985ea5ac5733a66416d91fac2f";
 
-  var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=metric" + "&APPID=f0f1cd985ea5ac5733a66416d91fac2f",
+   var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=metric" + "&APPID=f0f1cd985ea5ac5733a66416d91fac2f",
 
-  $('#searchWeather').click(function(){
-  var city = $("#city-input").val();
-    if(city != ''){
-      $.ajax({
-        url: queryURL,
-        method: "GET",
-        dataType: "json",
-        success: function(data){
-          console.log(data);
-        }
-  });
-    }else{
-      $("#error").html('Field cannot be blank');
-    }
+   $('#searchWeather').click(function(){
+   var city = $("#city-input").val();
+     if(city != ''){
+       $.ajax({
+         url: queryURL,
+         method: "GET",
+         dataType: "json",
+         success: function(data){
+           console.log(data);
+         }
+   });
+     }else{
+       $("#error").html('Field cannot be blank');
+     }
 
 
-    // DOM Variables
+     // DOM Variables
   
-    // JavaScript Variables
+     // JavaScript Variables
   
-    // Function Definitions
+     // Function Definitions
   
-    // Function Calls
+     // Function Calls
   
-    // Event Listeners
+     // Event Listeners
 
-    function alertCityName() {
-      var city = $(this).data("name");
-      alert(city);
-    }
+     function alertCityName() {
+       var city = $(this).data("name");
+       alert(city);
+     }
 
-    function renderButtons() {
+     function renderButtons() {
 
-      $("#submit").empty();
+       $("#submit").empty();
 
-      for (var i = 0; i < city.length; i++) {
-        var a = $("<button>");
-        a.addClass("city");
-        a.attr("data-name", city[i]);
-        a.text(city[i]);
-        $("submit").append(a);
-      }
-    }
+       for (var i = 0; i < city.length; i++) {
+         var a = $("<button>");
+         a.addClass("city");
+         a.attr("data-name", city[i]);
+         a.text(city[i]);
+         $("submit").append(a);
+       }
+     }
 
-    $("#add-city").on("click", function(event) {
-      event.preventDefault();
-      var city = $("search").val().trim();
-      city.push(city);
-      renderButtons();
-    });
+     $("#add-city").on("click", function(event) {
+       event.preventDefault();
+       var city = $("search").val().trim();
+       city.push(city);
+       renderButtons();
+     });
 
 
   
-  // We then created an AJAX call
+   // We then created an AJAX call
 
-      // $(".city").text("City: " + response.name);
-      // $(".wind").text("Wind Speed: " + response.wind.speed);
-      // $(".humidity").text("Humidity: " + response.main.humidity);
-      // $(".temp").text("Temperature: " + response.main.temp);
+       // $(".city").text("City: " + response.name);
+       // $(".wind").text("Wind Speed: " + response.wind.speed);
+       // $(".humidity").text("Humidity: " + response.main.humidity);
+       // $(".temp").text("Temperature: " + response.main.temp);
   
     
-      // AJAX GOES HERE LAST!
-      });
+       // AJAX GOES HERE LAST!
+       });
 
-      $(" .description").val(localStorage.getItem(" "))
+       $(" .description").val(localStorage.getItem(" "))
 
-    $(document).on("click", ".city", alertCityName);
+     $(document).on("click", ".city", alertCityName);
 
-    renderButtons();
+     renderButtons();
